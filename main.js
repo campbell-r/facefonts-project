be25510bdea75d5bf7871b0de879810ca063b77a
campbell-r <campbell.row@gmail.com>
2019-05-15 18:03:50 -0600

    adds all initial files

---
+++ main.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+  var video = document.getElementById('video');
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  var filter1 = document.getElementById('filter1');
+  var filter2 = document.getElementById('filter2');
+  var filter3 = document.getElementById('filter3');
+  var img = document.createElement('img');
+filter1.addEventListener('click', function(){
+  img.src = 'https://data.whicdn.com/images/272109778/large.png';
+});
+filter2.addEventListener('click', function(){
+  img.src = 'examples/assets/bald.png';
+});
+filter3.addEventListener('click', function(){
+  img.src = 'examples/assets/horns.png';
+});
+  var tracker = new tracking.ObjectTracker(['face']);
+  tracker.setInitialScale(4);
+  tracker.setStepSize(2);
+  tracker.setEdgesDensity(0.1);
+  tracking.track('#video', tracker, { camera: true });
+  tracker.on('track', function(event) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    event.data.forEach(function(rect) {
+      context.drawImage(img, rect.x, rect.y/4, rect.width + 15, rect.height * 1.9);
+
+    });
+  });
+};
